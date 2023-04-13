8915880964eb9fdbeafd7f54fc639df414df088c
Monalisa Rolim <monalisarolimsrodrigues@gmail.com>
2023-04-12 21:45:11 -0300

    exercicio de operdadore

---
+++ index.js
@@ -0,0 +1,38 @@
+/* let kelvin = (77 - 32) * (5/9) + 273.15;
+
+console.log (kelvin + "°K");*/
+
+//---------------------------------------
+
+/*let fahrenheit = (80 * (9/5) + 32);
+console.log(fahrenheit + "°F");*/
+
+//---------------------------------------
+
+/*let kelvin = (30 - 32) * (5/9) + 273.15;
+let fahrenheit = ((30 * (9/5)) + 32);
+
+console.log(fahrenheit + "°F, " + kelvin + "°K");*/
+
+//---------------------------------------
+
+/*let celsius = parseFloat(prompt("Digite o valor do grau celsius!"));
+
+kelvin = (celsius - 32) * (5/9) + 273.15;
+fahrenheit = ((celsius * (9/5)) + 32);
+
+console.log(fahrenheit + "°F, " + kelvin + "°K");*/
+
+//---------------------------------------
+
+/*let quilowatts = 280;
+let consumoEnergia = quilowatts * 0.05;
+
+console.log(consumoEnergia);*/
+
+//---------------------------------------
+
+let quilowatts = 280;
+let consumoEnergia = (quilowatts * 0.05) - 2.1;
+
+console.log(consumoEnergia);
